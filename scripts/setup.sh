1bc227635b5cf86961ca5dc940bf1349319d2d29
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2024-05-14 14:03:35 +0000

    chore(vscode): add launch options to debug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,9 @@ set -e
 cd "$(dirname "$0")/.."
 
 python3 -m pip install --requirement requirements.txt
+
+# Create config dir if not present
+if [[ ! -d "${PWD}/config" ]]; then
+    mkdir -p "${PWD}/config"
+    hass --config "${PWD}/config" --script ensure_config
+fi
