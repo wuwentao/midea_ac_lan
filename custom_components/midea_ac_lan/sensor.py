1265537530a0803d09ae03c707400ee2696990d7
Simone Chemelli <simone.chemelli@gmail.com>
2024-05-30 16:40:59 +0000

    chore: more typing

--- custom_components/midea_ac_lan/sensor.py
+++ custom_components/midea_ac_lan/sensor.py
@@ -1,17 +1,29 @@
+from typing import Any, cast
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import CONF_DEVICE_ID, CONF_SENSORS, Platform
 
 from .const import DEVICES, DOMAIN
 from .midea_devices import MIDEA_DEVICES
 from .midea_entity import MideaEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
+from homeassistant.helpers.typing import StateType
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     device_id = config_entry.data.get(CONF_DEVICE_ID)
     device = hass.data[DOMAIN][DEVICES].get(device_id)
     extra_sensors = config_entry.options.get(CONF_SENSORS, [])
     sensors = []
-    for entity_key, config in MIDEA_DEVICES[device.device_type]["entities"].items():
+    for entity_key, config in cast(
+        dict, MIDEA_DEVICES[device.device_type]["entities"]
+    ).items():
         if config["type"] == Platform.SENSOR and entity_key in extra_sensors:
             sensor = MideaSensor(device, entity_key)
             sensors.append(sensor)
@@ -20,21 +32,21 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
 class MideaSensor(MideaEntity, SensorEntity):
     @property
-    def native_value(self):
-        return self._device.get_attribute(self._entity_key)
+    def native_value(self) -> StateType:
+        return cast(StateType, self._device.get_attribute(self._entity_key))
 
     @property
-    def device_class(self):
-        return self._config.get("device_class")
+    def device_class(self) -> SensorDeviceClass:
+        return cast(SensorDeviceClass, self._config.get("device_class"))
 
     @property
-    def state_class(self):
-        return self._config.get("state_class")
+    def state_class(self) -> SensorStateClass | None:
+        return cast(SensorStateClass | None, self._config.get("state_class"))
 
     @property
-    def native_unit_of_measurement(self):
-        return self._config.get("unit")
+    def native_unit_of_measurement(self) -> str | None:
+        return cast(str | None, self._config.get("unit"))
 
     @property
-    def capability_attributes(self):
+    def capability_attributes(self) -> dict[str, Any] | None:
         return {"state_class": self.state_class} if self.state_class else {}
--- custom_components/midea_ac_lan/switch.py
+++ custom_components/midea_ac_lan/switch.py
@@ -1,17 +1,27 @@
+from typing import Any, cast
 from homeassistant.const import CONF_DEVICE_ID, CONF_SWITCHES, Platform
 from homeassistant.helpers.entity import ToggleEntity
 
 from .const import DEVICES, DOMAIN
 from .midea_devices import MIDEA_DEVICES
 from .midea_entity import MideaEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     device_id = config_entry.data.get(CONF_DEVICE_ID)
     device = hass.data[DOMAIN][DEVICES].get(device_id)
     extra_switches = config_entry.options.get(CONF_SWITCHES, [])
     switches = []
-    for entity_key, config in MIDEA_DEVICES[device.device_type]["entities"].items():
+    for entity_key, config in cast(
+        dict, MIDEA_DEVICES[device.device_type]["entities"]
+    ).items():
         if config["type"] == Platform.SWITCH and entity_key in extra_switches:
             dev = MideaSwitch(device, entity_key)
             switches.append(dev)
@@ -21,10 +31,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class MideaSwitch(MideaEntity, ToggleEntity):
     @property
     def is_on(self) -> bool:
-        return self._device.get_attribute(self._entity_key)
+        return cast(bool, self._device.get_attribute(self._entity_key))
 
-    def turn_on(self):
+    def turn_on(self, **kwargs: Any) -> None:
         self._device.set_attribute(attr=self._entity_key, value=True)
 
-    def turn_off(self):
+    def turn_off(self, **kwargs: Any) -> None:
         self._device.set_attribute(attr=self._entity_key, value=False)
