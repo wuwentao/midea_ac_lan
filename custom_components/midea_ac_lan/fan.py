a83d5f37f17debae393b1f30c1a80bb0f2db76a9
Necroneco <caibinqing@live.com>
2024-06-04 14:42:11 +0800

    Update fan.py

--- custom_components/midea_ac_lan/fan.py
+++ custom_components/midea_ac_lan/fan.py
@@ -218,7 +218,7 @@ class Midea40Fan(MideaFan):
 
     @property
     def is_on(self) -> bool:
-        return self._device.get_attribute(attr=X40Attributes.fan_speed) > 0
+        return cast(int, self._device.get_attribute(attr=X40Attributes.fan_speed)) > 0
 
     def turn_on(
         self,
