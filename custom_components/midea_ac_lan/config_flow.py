b37993089d2fcd730c61b8f83d1bf658fa776b3c
Simone Chemelli <simone.chemelli@gmail.com>
2024-06-21 16:43:15 +0900

    chore: fix ruff B007 (#110)
    Co-authored-by: Hello World <wuwentao2000@126.com>

--- custom_components/midea_ac_lan/config_flow.py
+++ custom_components/midea_ac_lan/config_flow.py
@@ -370,7 +370,7 @@ class MideaLanConfigFlow(ConfigFlow, domain=DOMAIN):
                     if not await self.cloud.login():
                         return await self.async_step_auto(error="preset_account")
                 keys = await self.cloud.get_keys(user_input[CONF_DEVICE])
-                for method, key in keys.items():
+                for _, key in keys.items():
                     dm = MideaDevice(
                         name="",
                         device_id=device_id,
