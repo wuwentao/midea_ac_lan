721591b18b55493e95c674de261f9de982928b63
Simone Chemelli <simone.chemelli@gmail.com>
2024-06-21 20:36:18 +0900

    chore(switch): fix ruff D10x (#125)

--- custom_components/midea_ac_lan/switch.py
+++ custom_components/midea_ac_lan/switch.py
@@ -1,3 +1,5 @@
+"""Switch for Midea Lan."""
+
 from typing import Any, cast
 
 from homeassistant.config_entries import ConfigEntry
@@ -16,6 +18,7 @@ async def async_setup_entry(
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
+    """Set up switches for device."""
     device_id = config_entry.data.get(CONF_DEVICE_ID)
     device = hass.data[DOMAIN][DEVICES].get(device_id)
     extra_switches = config_entry.options.get(CONF_SWITCHES, [])
@@ -31,12 +34,17 @@ async def async_setup_entry(
 
 
 class MideaSwitch(MideaEntity, ToggleEntity):
+    """Represent a Midea switch."""
+
     @property
     def is_on(self) -> bool:
+        """Return true if switch is on."""
         return cast(bool, self._device.get_attribute(self._entity_key))
 
     def turn_on(self, **kwargs: Any) -> None:  # noqa: ANN401, ARG002
+        """Turn on switch."""
         self._device.set_attribute(attr=self._entity_key, value=True)
 
     def turn_off(self, **kwargs: Any) -> None:  # noqa: ANN401, ARG002
+        """Turn off switch."""
         self._device.set_attribute(attr=self._entity_key, value=False)
