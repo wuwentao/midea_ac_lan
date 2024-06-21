a2b830e14fdc46169e4ce567a2604edd227201bd
Simone Chemelli <simone.chemelli@gmail.com>
2024-06-21 22:41:36 +0900

    chore: fix ruff ARG001 (#129)

--- custom_components/midea_ac_lan/__init__.py
+++ custom_components/midea_ac_lan/__init__.py
@@ -73,7 +73,7 @@ async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> Non
             dev.set_refresh_interval(refresh_interval)
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:  # noqa ARG001
     """Setup midea_lan component when load this integration"""
     hass.data.setdefault(DOMAIN, {})
     attributes = []
