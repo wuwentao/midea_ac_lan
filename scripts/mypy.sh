bc88b845a5940a6f63a2e847f5359a224893d8b0
Wentao Wu <wuwentao@canaan-creative.com>
2024-06-25 10:56:30 +0800

    fix: update python to python3 in myph.sh

--- scripts/mypy.sh
+++ scripts/mypy.sh
@@ -4,7 +4,7 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-pyver=$(python -c 'import platform; major, minor, path = platform.python_version_tuple(); print(f"{major}.{minor}")')
+pyver=$(python3 -c 'import platform; major, minor, path = platform.python_version_tuple(); print(f"{major}.{minor}")')
 
 mypy --config-file mypy-$pyver.ini .
 
