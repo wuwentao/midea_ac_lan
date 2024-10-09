48e85c79ff9e385734c303b3f3ece3c1649fe1bc
Simone Chemelli <simone.chemelli@gmail.com>
2024-10-09 19:06:08 +0200

    build: add commitlint to pre-commit (#357)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,11 @@ cd "$(dirname "$0")/.."
 
 # Install pre-commit hooks on commit
 pre-commit install
+pre-commit install --hook-type commit-msg
+
+cd
+npm install @commitlint/config-conventional
+cd "$(dirname "$0")/.."
 
 # Create config dir if not present
 if [[ ! -d "${PWD}/config" ]]; then
