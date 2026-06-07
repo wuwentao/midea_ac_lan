ea6385e7ce15a7028fd5fb4c2e27b29a59ce2027
Wentao Wu <wuwentao2000@126.com>
2026-06-07 17:55:31 +0800

    fix(ci): midea-local remove python 3.11 suppoprt

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [2, "always", 300],
+    "body-max-line-length": [2, "always", 500],
+    "scope-enum": [0],
   },
 };
